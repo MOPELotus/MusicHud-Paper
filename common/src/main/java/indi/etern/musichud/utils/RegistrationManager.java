bd1e7d215f4a36edbe924bd63b1310a5c7bae194
SummerLotus520 <2402804043@qq.com>
2026-03-21 18:07:40 +0800

    Fix Fabric client keybind registration

--- common/src/main/java/indi/etern/musichud/utils/RegistrationManager.java
+++ common/src/main/java/indi/etern/musichud/utils/RegistrationManager.java
@@ -63,11 +63,15 @@ public class RegistrationManager {
 
         // 根据环境注册特定接口
         if (envType == EnvType.CLIENT) {
-            registerClassesFromList(CLIENT_REGISTRIES, "client");
+            performClientRegistration();
         } else {
             registerClassesFromList(SERVER_REGISTRIES, "server");
         }
         registerClassesFromList(COMMON_REGISTRIES, "common");
+    }
+
+    public static void performClientRegistration() {
+        registerClassesFromList(CLIENT_REGISTRIES, "client");
     }
 
     private static void registerClassesFromList(String[] classNames, String typeName) {
