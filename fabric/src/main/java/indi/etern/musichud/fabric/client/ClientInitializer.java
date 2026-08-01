cea57f0f3a5c8d3cd9984fb151a4b0c7b2b4a683
MOPELotus <79179977+MOPELotus@users.noreply.github.com>
2026-08-02 03:32:54 +0800

    Adapt self-use features to 1.21.1

--- fabric/src/main/java/indi/etern/musichud/fabric/client/ClientInitializer.java
+++ fabric/src/main/java/indi/etern/musichud/fabric/client/ClientInitializer.java
@@ -1,18 +1,13 @@
 package indi.etern.musichud.fabric.client;
 
-import indi.etern.musichud.MusicHud;
 import indi.etern.musichud.client.ui.hud.HudRendererManager;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElementRegistry;
-import net.minecraft.resources.ResourceLocation;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 
 public final class ClientInitializer implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         HudRendererManager hudRendererManager = HudRendererManager.getInstance();
-        HudElementRegistry.addFirst(
-                ResourceLocation.fromNamespaceAndPath(MusicHud.MOD_ID, "main_hud"),
-                hudRendererManager::renderFrame
-        );
+        HudRenderCallback.EVENT.register(hudRendererManager::renderFrame);
     }
 }
